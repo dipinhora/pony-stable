81ac1846caea48ee0dc2e286f55fbc51b61a1f6a
Dipin Hora <dipin@wallaroolabs.com>
2018-07-25 01:21:55 +0000

    m

--- .travis_install.bash
+++ .travis_install.bash
@@ -7,7 +7,7 @@ install_ponyc(){
   echo -e "\033[0;32mInstalling latest ponyc release\033[0m"
   sudo add-apt-repository ppa:ponylang/ponylang -y
   sudo apt-get update
-  sudo apt-get -V install ponyc libpcre2-dev -y
+  sudo apt-get -V install -f ponyc -y
 }
 
 install_ponyc
